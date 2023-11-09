6471595e67f367ae4cc9b2eb14bcc3a0bbef7e6e
Mohamed Salem <Eng_freelance@outlook.com>
2023-11-09 06:12:41 +0200

    commit

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import os
 from random import randint
 
+# List of files to monitor
+monitored_files = ['App.vue', 'Main.vue', 'main.js']
 
-for i in range(1, 365):
+for _ in range(randint(1, 10)):
+    for i in range(1, 365):
+        d = str(i) + 'days ago'
+        with open('file.txt', 'a') as file:
+            file.write(d)
 
-  for j in range(0, randint(1, 10)):
-    d = str(i) + 'days ago'
-    with open('file.txt', 'a') as file:
-        file.write(d)
-    os.system('git add .')
-    os.system('git commit --date="' + d + '" -m "commit"')
+        os.system('git add .')
+        os.system('git commit --date="' + d + '" -m "commit"')
 
 os.system('git push -u origin main')
