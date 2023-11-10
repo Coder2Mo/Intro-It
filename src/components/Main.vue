05c0dbe3f331e5e8a967034d8d6b16e7691390a8
Mohamed Salem <Eng_freelance@outlook.com>
2023-11-10 03:53:49 +0200

    Update Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="Main">
+  <div class="main">
     <div id="demo">
       <div id="inner-demo">
         <h1 id="inner-demo-2">
-          <br>
+          <br />
         </h1>
       </div>
     </div>
-    <VueParticle domId="demo" :config="particleConfig"/>
+    <VueParticle domId="demo" :config="particleConfig" />
   </div>
 </template>
 
@@ -16,10 +16,10 @@ import VueParticle from "vue-particlejs";
 export default {
   name: "Main",
   props: {
-    msg: String
+    msg: String,
   },
   components: {
-    VueParticle
+    VueParticle,
   },
   data() {
     return {
@@ -31,24 +31,24 @@ export default {
             type: "circle",
             stroke: { width: 0, color: "#000000" },
             polygon: { nb_sides: 5 },
-            image: { src: "img/github.svg", width: 100, height: 100 }
+            image: { src: "img/github.svg", width: 100, height: 100 },
           },
           opacity: {
             value: 0.5,
             random: false,
-            anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+            anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
           },
           size: {
             value: 5,
             random: true,
-            anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+            anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
           },
           line_linked: {
             enable: true,
             distance: 150,
             color: "#555555",
             opacity: 0.4,
-            width: 3
+            width: 3,
           },
           move: {
             enable: true,
@@ -58,15 +58,15 @@ export default {
             straight: false,
             out_mode: "out",
             bounce: false,
-            attract: { enable: false, rotateX: 600, rotateY: 1200 }
-          }
+            attract: { enable: false, rotateX: 600, rotateY: 1200 },
+          },
         },
         interactivity: {
           detect_on: "canvas",
           events: {
             onhover: { enable: false, mode: "repulse" },
             onclick: { enable: false, mode: "push" },
-            resize: true
+            resize: true,
           },
           modes: {
             grab: { distance: 400, line_linked: { opacity: 1 } },
@@ -75,17 +75,17 @@ export default {
               size: 40,
               duration: 2,
               opacity: 8,
-              speed: 3
+              speed: 3,
             },
             repulse: { distance: 200, duration: 0.4 },
             push: { particles_nb: 4 },
-            remove: { particles_nb: 2 }
-          }
+            remove: { particles_nb: 2 },
+          },
         },
-        retina_detect: true
-      }
+        retina_detect: true,
+      },
     };
-  }
+  },
 };
 </script>
 
