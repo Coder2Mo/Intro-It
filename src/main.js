06181628272f0690f7238cb76d3c357126a305b5
Mohamed Salem <Eng_freelance@outlook.com>
2023-11-12 05:18:29 +0200

    Update .gitattributes for line endings

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
+/*
+ * Intro-It Project - Create a GitHub Profile Readme Intro
+ *
+ * Intro-It is a project designed to craft compelling intros for your GitHub profile readme.md.
+ * It aims to provide an engaging and interactive introduction to make your profile stand out.
+ *
+ * Project Repository: https://github.com/Coder2Mo/Intro-It
+ *
+ * Author: Mohamed Salem
+ * Email: Eng_freelance@outlook.com
+ * Copyright (C) 2023 Mohamed Salem. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import Vue from "vue";
 import App from "./App.vue";
-import MakeIt from "introit";
+import TypeIt from "typeit";
 
 Vue.config.productionTip = false;
 
@@ -8,8 +22,8 @@ new Vue({
   render: (h) => h(App)
 }).$mount("#app");
 
-// Initialize IntroIt for a professional Intro Writter effect
-new MakeIt("#demo2", {
+// Initialize TypeIt for a professional Intro Writer effect
+new TypeIt("#inner-demo-2", {
   speed: 100,
   lifelike: true,
   cursor: true,
